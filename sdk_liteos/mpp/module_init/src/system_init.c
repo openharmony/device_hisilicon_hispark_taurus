fa3e728842125b095f0af1310dcc8d6341def945
juchenyang <juchenyang@huawei.com>
2021-07-29 03:40:55 +0000

    Description:modify network port driver code. Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: juchenyang <juchenyang@huawei.com>

--- sdk_liteos/mpp/module_init/src/system_init.c
+++ sdk_liteos/mpp/module_init/src/system_init.c
@@ -74,38 +74,11 @@
 
 void net_init(void)
 {
-    struct netif *pnetif;
-
 extern void tcpip_init(tcpip_init_done_fn initfunc, void *arg);
     dprintf("\ntcpip_init start\n");
     tcpip_init(NULL, NULL);
     dprintf("\ntcpip_init end\n");
-
-    static unsigned int overtime = 0;
     PRINTK("Ethernet start.");
-
-#ifdef LOSCFG_DRIVERS_HIGMAC
-    extern int ethnet_higmac_init(void);
-    (void)ethnet_higmac_init();
-#endif
-
-#ifdef LOSCFG_DRIVERS_HIETH_SF
-    extern int ethnet_hieth_init(void);
-    (void)ethnet_hieth_init();
-#endif
-
-    extern void get_defaultNetif(struct netif **pnetif);
-    get_defaultNetif(&pnetif);
-
-    (void)netifapi_netif_set_up(pnetif);
-    do {
-        LOS_Msleep(SLEEP_TIME_MS);
-        overtime++;
-        if (overtime > NETIF_SETUP_OVERTIME) {
-            PRINTK("netif_is_link_up overtime!\n");
-            break;
-        }
-    } while (netif_is_link_up(pnetif) == 0);
 }
 #endif
 
